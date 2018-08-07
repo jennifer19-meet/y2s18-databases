d47f72ec92b2f4aa17a8527e23ada25db6fa208c
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 12:07:04 +0300

    s

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -17,27 +17,49 @@ def add_article(topic, title, rating):
 	session.commit()
 
 
-# add_article("shiraz", "goat songs", 10000)
+add_article("shiraz", "goat songs", 10000)
 
 # print(repr(Knowledge.__table__))
 def query_all_articles():
 	q = session.query(Knowledge
 		).all()
+	
 	return(q)
 	
-print(query_all_articles())
+#print(query_all_articles())
 def query_article_by_topic(topic):
 
 	fil = session.query(Knowledge
 		).filter_by(
 		topic= topic).first()
+	
 	return (fil)
-print(query_article_by_topic("shir"))
-# def delete_article_by_topic():
+# print(query_article_by_topic("shir"))
+def delete_article_by_topic(topic):
+	hh = session.query(Knowledge
+		).filter_by(
+		topic = topic).delete()
+	session.commit()
 
-
-# def delete_all_articles():
-
-
-# def edit_article_rating():
-# 	pass
+# delete_article_by_topic("shiraz")
+def delete_all_articles():
+	hj = session.query(Knowledge
+		).filter_by().delete()
+	session.commit()
+	
+# delete_all_articles()
+def edit_article_rating(topic, new_rating):
+	artic = session.query(Knowledge
+		).filter_by(
+		topic = topic).first()
+	artic.rating = new_rating
+	session.commit()
+def too_low(threshold):
+	rat = session.query(Knowledge
+		).filter_by(threshold=threshold)
+	if rat.rating< threshold:
+		rat.delete()
+	
+edit_article_rating("shiraz", 10)
+too_low(11)
+print(query_all_articles())
